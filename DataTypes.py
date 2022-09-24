026ee990472b5b03bd5e86f7d9516ed333fae6a2
MD Shakil Mahmud Shohag <70774510+shakilmahmudshohag@users.noreply.github.com>
2022-09-24 14:44:28 +0600

    Update DataTypes.py

--- DataTypes.py
+++ DataTypes.py
@@ -994,7 +994,30 @@ def to_title(string):
 
 
 print(to_title("I will love you forever Meri moonlight"))
-"""
+
 
 
 #Python | Swap commas and dots in a String
+# Python code to replace, with . and vice-versa
+def Replace(str1):
+    maketrans = str1.maketrans
+    final = str1.translate(maketrans(',.', '.,', ' '))
+    return final.replace(',', ", ")
+
+
+# Driving Code
+string = "14, 625, 498.002"
+print(Replace(string))
+
+
+#using replace()
+def Replace(str1):
+    str1 = str1.replace(', ', 'third')
+    str1 = str1.replace('.', ', ')
+    str1 = str1.replace('third', '.')
+    return str1
+
+
+string = "14, 625, 498.002"
+print(Replace(string))
+"""
